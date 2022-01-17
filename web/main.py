75c8940f74d3fe30d95497c38349fb3f705d9483
yyyyuwen <circus61029@gmail.com>
2022-01-17 20:39:11 +0800

    web : add tf-idf analysis

--- web/main.py
+++ web/main.py
@@ -77,12 +77,30 @@ def index():
         return render_template("index.html", full = full, KeyWord = KeyWord, rankDoc = rankDoc, find = find, cate = cate)
     else:
         KeyWord = ""
-        return render_template("index.html", full = full, KeyWord = KeyWord)        
+        return render_template("index.html", full = full, KeyWord = KeyWord, cate = cate)
+
+@app.route("/show", methods=['POST','GET'])
+def show():
+    title = ""
+    with open('../data/full.pkl', 'rb')as fpick:
+        full = pickle.load(fpick)
+    with open('../data/Category.pkl', 'rb')as fpick:
+        cate = pickle.load(fpick)
+    if request.method == "POST":
+        title = request.form["Article"]
+    return render_template("show.html", full = full, cate = cate, title = title, KeyWord = "")
+
+@app.route("/overview", methods=['POST','GET'])
+def overview():
+    return render_template("overview.html", KeyWord = "")
+
 #-----------------Article Search --------------------------#
 
 #-----------------   Analysis    --------------------------#
 @app.route("/about", methods=['POST','GET'])
 def about():
+    with open('../data/kmeans_result.pkl', 'rb')as fpick:
+        km = pickle.load(fpick)
     model = Word2Vec.load("../script/word2vec_cbow.model")
     word_vectors_model = model.wv
     user_input = ['heart', 'coronavirus', 'pneumonia', 'fever', 'cough']
@@ -96,8 +114,47 @@ def about():
     data_json= display_tsne_scatterplot_2D(word_vectors_model, user_input, similar_word, labels, color_map, topn, 5, 500, 10000)
         # 將圖表存成json
     dataJSON = json.dumps(data_json, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template("about.html", **locals())
+    km_json= display_kmeans(km)
+        # 將圖表存成json
+    kmJSON = json.dumps(km_json, cls=plotly.utils.PlotlyJSONEncoder)
 
+    return render_template("about.html", **locals(), KeyWord = "")
+
+def display_kmeans(km):
+    data = []
+    cluster = ["Cluster 1: heart, ventricular, echocardiography, coronary, myocardial",
+    "Cluster 2: cov, test, assay, sample, rt, detection",
+    "Cluster 3: covid19, sars, pcr, coronavirus, infection",
+    "Cluster 4: pneumoniae, mycoplasma, infection, pathogen, case",
+    "Cluster 5: diagnosis, lung, clinical, chest, pulmonary, bacterial"]
+    for i in range(len(km)):
+        trace = go.Scatter(
+            x = [item[0] for item in km[i]], 
+            y = [item[1] for item in km[i]],
+            text = [item[2] for item in km[i]],
+            name = cluster[i],
+            mode = 'markers',
+            marker = {
+                'size': 10,
+                'opacity': 0.8,
+                'color' : i
+            },    
+        )
+        data.append(trace)
+
+    layout = go.Layout(
+        showlegend=True,
+        legend=dict(
+        x=0,
+        y=0),
+        margin = {'l': 0, 'r': 0, 'b': 0, 't': 0},
+        width = 1000,
+        height = 1000
+    )
+    plot_figure = go.Figure(data = data, layout = layout)
+    show_data = [plot_figure]
+
+    return show_data
 
 def display_tsne_scatterplot_2D(model, user_input=None, words=None, label=None, color_map=None, topn=None, perplexity = 0, learning_rate = 0, iteration = 0, sample=10):
     if words == None:
@@ -151,22 +208,9 @@ def display_tsne_scatterplot_2D(model, user_input=None, words=None, label=None, 
     # Configure the layout
     layout = go.Layout(
         margin = {'l': 0, 'r': 0, 'b': 0, 't': 0},
-        showlegend=True,
-        legend=dict(
-        x=1,
-        y=0.5,
-        font=dict(
-            family="Courier New",
-            size=25,
-            color="black"
-        )),
-        font = dict(
-            family = " Courier New ",
-            size = 15),
-        autosize = False,
         width = 1000,
         height = 1000
-        )
+    )
 
     plot_figure = go.Figure(data = data, layout = layout)
     # print(plot_figure)
@@ -176,4 +220,4 @@ def display_tsne_scatterplot_2D(model, user_input=None, words=None, label=None, 
 
 #-----------------   Analysis    --------------------------#
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host = '0.0.0.0', port='8080')
